84aa9491dd48d48832c105f53103220f09bba3e1
Artur <artur.prado@gmail.com>
2017-02-04 15:34:01 -0200

    Load map only once

--- app/assets/javascripts/modules/landing/maps.js
+++ app/assets/javascripts/modules/landing/maps.js
@@ -1,2 +1,16 @@
 $(document).ready(function() {
+  var waypointsOptions = {
+    offset: 'bottom-in-view'
+  };
+
+  $('#maps-visibility').waypoint(function(direction) {
+    var elem = $('#maps-visibility'),
+        src = elem.data('src'),
+        loaded = elem.data('loaded');
+
+    if (!loaded) {
+      $('#google-maps').attr('src', src);
+      elem.attr('data-loaded', true);
+    }
+  }, waypointsOptions);
 });
