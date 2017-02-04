798137a5199e75748b1337984ce9525998c06720
Artur <artur.prado@gmail.com>
2017-02-04 14:38:39 -0200

    Start cleaning up the scripts

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module Arsenal
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = 'pt-BR'
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
