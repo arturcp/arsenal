6f49111c5b6509204a65b7cca4b30eb5da84cf7e
Artur <artur.prado@gmail.com>
2017-02-04 17:32:57 -0200

    Read magnific popup from bower

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -14,6 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require materialize
-//= require_tree ./modules/libs
 //= require waypoints/lib/jquery.waypoints
+//= require magnific-popup
 //= require_tree ./modules/landing
