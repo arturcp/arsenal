47099a8ee93d3826a851c5e13844dcd87732bdc6
Artur <artur.prado@gmail.com>
2017-04-27 08:46:28 -0300

    Include modal

---
+++ app/assets/javascripts/modules/landing/modal.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // var modal = $('#closedCampaignModal');
+  //
+  // if (modal.length > 0) {
+  //   modal.modal('open');
+  // }
+
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal').modal();
+});
