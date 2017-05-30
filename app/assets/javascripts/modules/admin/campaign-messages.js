ef4bb6a9a9b4e3b728c3a6d42e5f334957472542
Artur <artur.prado@gmail.com>
2017-05-29 21:09:32 -0300

    Transfer rows between tabs when approving or rejecting campaign messages

--- app/assets/javascripts/modules/admin/campaign-messages.js
+++ app/assets/javascripts/modules/admin/campaign-messages.js
@@ -1,6 +1,6 @@
 define('campaign-messages', [], function() {
   function CampaignMessages() {
-    this.buttons = $('.centralized-badge');
+    this.badges = $('.centralized-badge');
     this.modal = $('#campaign-messages-modal');
 
     this._bindEvents();
@@ -9,7 +9,13 @@ define('campaign-messages', [], function() {
   var fn = CampaignMessages.prototype;
 
   fn._bindEvents = function() {
-    this.buttons.on('click', $.proxy(this._openModal, this));
+    this.badges.off('click').on('click', $.proxy(this._openModal, this));
+
+    var approveButtons = $('[data-approve-message]'),
+        rejectButtons = $('[data-reject-message]');
+
+    approveButtons.off('click').on('click', $.proxy(this._approveMessage, this));
+    rejectButtons.off('click').on('click', $.proxy(this._rejectMessage, this));
   };
 
   fn._openModal = function(event) {
@@ -22,9 +28,61 @@ define('campaign-messages', [], function() {
     }).done(function(content) {
       $('[data-messages-tabs]').html(content);
       self.modal.modal().modal('open');
-      $('#campaign-messages-modal .tabs').tabs();
-      $('#campaign-messages-modal .tabs a:first').trigger('click');
+
+      var tabs = $('#campaign-messages-modal .tabs'),
+          firstTab = $('#campaign-messages-modal .tabs a:first');
+
+      tabs.tabs();
+      firstTab.trigger('click');
+
+      self._bindEvents();
     });
+  };
+
+  fn._changeMessageStatus = function(event, status) {
+    var element = $(event.currentTarget),
+        messageId = element.data(status + '-message');
+        row = element.parents('tr:first');
+
+    new RowTransferer(row).transferTo(status);
+
+    this._bindEvents();
+  };
+
+  fn._approveMessage = function(event) {
+    this._changeMessageStatus(event, 'approve');
+  };
+
+  fn._rejectMessage = function(event) {
+    this._changeMessageStatus(event, 'reject');
+  };
+
+  var RowTransferer = function(row, status) {
+    this.row = row;
+    this.hideClass = 'hide';
+
+    this.transferTo = function(status) {
+      this.status = status;
+      this.container = $('[data-' + status + '-container]')
+
+      var newRow = this._transferRow();
+      this._toggleButtons(newRow);
+      this.row.hide('slow', function() { $(this).remove() });
+    };
+
+    this._toggleButtons = function(row) {
+      triggeredButton = row.find('.' + this.status + '-message');
+
+      row.find('.hide').removeClass(this.hideClass);
+      triggeredButton.addClass(this.hideClass);
+    };
+
+    this._transferRow = function() {
+      var newRow = $(this.row.prop('outerHTML'));
+      this.container.find('table tbody').prepend(newRow);
+
+      return newRow;
+    };
   };
 
   return CampaignMessages;
